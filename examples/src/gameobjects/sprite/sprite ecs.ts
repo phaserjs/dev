7c931a84e164bdac16e31fbbc9c5e698ee20cbcc
Richard Davey <rich@photonstorm.com>
2021-05-20 21:03:50 +0100

    Update sprite ecs.ts

--- examples/src/gameobjects/sprite/sprite ecs.ts
+++ examples/src/gameobjects/sprite/sprite ecs.ts
@@ -4,10 +4,13 @@ import { AddChild } from '../../../../../phaser-genesis/src/display/';
 import { Game } from '../../../../../phaser-genesis/src/Game';
 import { ImageFile } from '../../../../../phaser-genesis/src/loader/files/ImageFile';
 import { Loader } from '../../../../../phaser-genesis/src/loader/Loader';
+import { LocalMatrix2DComponent } from '../../../../../phaser-genesis/src/components/transform/LocalMatrix2DComponent';
 import { On } from '../../../../../phaser-genesis/src/events';
 import { Scene } from '../../../../../phaser-genesis/src/scenes/Scene';
 import { Sprite } from '../../../../../phaser-genesis/src/gameobjects/';
 import { StaticWorld } from '../../../../../phaser-genesis/src/world/StaticWorld';
+import { Transform2DComponent } from '../../../../../phaser-genesis/src/components/transform/Transform2DComponent';
+import { WorldMatrix2DComponent } from '../../../../../phaser-genesis/src/components/transform/WorldMatrix2DComponent';
 
 class Demo extends Scene
 {
@@ -27,13 +30,15 @@ class Demo extends Scene
             const rocket2 = new Sprite(200, 100, 'rocket');
             const rocket3 = new Sprite(600, 500, 'rocket');
 
-            console.log(rocket);
-            console.log(rocket2);
-            console.log(rocket3);
-
             AddChild(world, rocket);
             AddChild(world, rocket2);
             AddChild(world, rocket3);
+
+            window.addEventListener('mousedown', () => {
+
+                Transform2DComponent.x[world.id] += 10;
+
+            });
 
             On(this, 'update', () => {
 
