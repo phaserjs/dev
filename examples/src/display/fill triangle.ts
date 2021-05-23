f746cc86e6c871547e02e4ac8bb6b98e63604f24
Richard Davey <rich@photonstorm.com>
2021-05-23 18:27:42 +0100

    Triangle and Line tests

---
+++ examples/src/display/fill triangle.ts
@@ -0,0 +1,33 @@
+import { BackgroundColor, GlobalVar, Parent, Scenes, WebGL } from '../../../../phaser-genesis/src/config';
+
+import { FillTriangle } from '../../../../phaser-genesis/src/renderer/webgl1/draw/FillTriangle';
+import { Game } from '../../../../phaser-genesis/src/Game';
+import { IRenderPass } from '../../../../phaser-genesis/src/renderer/webgl1/renderpass/IRenderPass';
+import { On } from '../../../../phaser-genesis/src/events/On';
+import { Scene } from '../../../../phaser-genesis/src/scenes/Scene';
+import { StaticWorld } from '../../../../phaser-genesis/src/world/StaticWorld';
+import { WorldPostRenderEvent } from '../../../../phaser-genesis/src/world/events';
+
+class Demo extends Scene
+{
+    constructor ()
+    {
+        super();
+
+        const world = new StaticWorld(this);
+
+        On(world, WorldPostRenderEvent, (renderPass: IRenderPass) => {
+
+            FillTriangle(renderPass, 100, 100, 100, 200, 200, 200, 0x00ff00);
+
+        });
+    }
+}
+
+new Game(
+    WebGL(),
+    Parent('gameParent'),
+    GlobalVar('Phaser4'),
+    BackgroundColor(0x2d2d2d),
+    Scenes(Demo)
+);
