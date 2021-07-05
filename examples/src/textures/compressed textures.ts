0512b82b6bbee9ce52ac78ba05b435822f2bf672
Richard Davey <rich@photonstorm.com>
2021-07-05 18:19:07 +0100

    Compressed texture tests

--- examples/src/textures/compressed textures.ts
+++ examples/src/textures/compressed textures.ts
@@ -1,11 +1,15 @@
-import { BackgroundColor, GlobalVar, Parent, Scenes, WebGL } from '../../../../phaser-genesis/src/config';
+import { BackgroundColor, GlobalVar, Parent, Scenes, Size, WebGL } from '../../../../phaser-genesis/src/config';
+import { Sprite, Text } from '../../../../phaser-genesis/src/gameobjects/';
 
 import { AddChild } from '../../../../phaser-genesis/src/display/';
 import { Game } from '../../../../phaser-genesis/src/Game';
+import { GameInstance } from '../../../../phaser-genesis/src/GameInstance';
+import { GetRenderer } from '../../../../phaser-genesis/src/config/renderer';
+import { IWebGLRenderer } from '../../../../phaser-genesis/src/renderer/webgl1/IWebGLRenderer';
 import { Scene } from '../../../../phaser-genesis/src/scenes/Scene';
-import { Sprite } from '../../../../phaser-genesis/src/gameobjects/';
 import { StaticWorld } from '../../../../phaser-genesis/src/world/StaticWorld';
 import { TextureFile } from '../../../../phaser-genesis/src/loader/files';
+import { WebGLRenderer } from '../../../../phaser-genesis/src/renderer/webgl1';
 
 class Demo extends Scene
 {
@@ -18,13 +22,59 @@ class Demo extends Scene
 
     async create ()
     {
-        await TextureFile('test', 'assets/compressed/atlas-pvr3-dxt1.pvr').load();
+        /*
+        const d = new Text(10, 10, 'Hello World', '10px monospace');
+
+        d.backgroundStyle = '#000000';
+        d.lineSpacing = 2;
+
+        d.origin.set(0, 0);
+
+        const log = [];
+
+        for (const [ key, value ] of Object.entries(this.game.renderer.compression))
+        {
+            log.push(`${key}: ${value}`);
+
+            if (value !== null)
+            {
+                for (const key2 in value)
+                {
+                    log.push(`--- ${key2}`);
+                }
+            }
+        }
+
+        d.setText(log);
+        */
+
+        //  Works, but no alpha channel
+        // await TextureFile('test', 'assets/compressed/atlas-pvr-dxt1.pvr').load();
+
+        //  Works on Windows / Mac OS with alpha channel
+        // await TextureFile('test', 'assets/compressed/atlas-pvr-dxt5.pvr').load();
+
+        //  Android?
+        // await TextureFile('test', 'assets/compressed/atlas-pvr-etc2-rgba.pvr').load();
+
+        //  iOS
+        // await TextureFile('test', 'assets/compressed/atlas-pvr-pvrtci2bpp-rgba.pvr').load();
+        // await TextureFile('test', 'assets/compressed/atlas-pvr-pvrtci4bpp-rgba.pvr').load();
+        await TextureFile('test', 'assets/compressed/atlas-pvr-rgba8888.pvr').load();
+
+        const d = new Text(10, 10, '', '10px monospace');
+        d.origin.set(0, 0);
+        d.backgroundStyle = '#000000';
+        d.text = 'atlas-pvr-rgba8888.pvr';
 
         const world = new StaticWorld(this);
 
-        const sprite = new Sprite(400, 300, 'test');
+        const sprite = new Sprite(-200, 0, 'test');
+
+        sprite.origin.set(0, 0);
 
         AddChild(world, sprite);
+        AddChild(world, d);
     }
 }
 
@@ -32,6 +82,6 @@ new Game(
     WebGL(),
     Parent('gameParent'),
     GlobalVar('Phaser4'),
-    BackgroundColor(0x2d2d2d),
+    BackgroundColor(0xee2d2d),
     Scenes(Demo)
 );
