ae982c4a4c7509b919f810105944f88d6592c20e
Richard Davey <rich@photonstorm.com>
2021-07-07 18:23:17 +0100

    New DD examples

---
+++ examples/src/gameobjects/directdraw/draw rect.ts
@@ -0,0 +1,44 @@
+import { BackgroundColor, GlobalVar, Parent, Scenes, WebGL } from '../../../../../phaser-genesis/src/config';
+
+import { AddChild } from '../../../../../phaser-genesis/src/display';
+import { DirectDraw } from '../../../../../phaser-genesis/src/gameobjects';
+import { Game } from '../../../../../phaser-genesis/src/Game';
+import { ImageFile } from '../../../../../phaser-genesis/src/loader/files';
+import { Scene } from '../../../../../phaser-genesis/src/scenes/Scene';
+import { StaticWorld } from '../../../../../phaser-genesis/src/world/StaticWorld';
+
+class Demo extends Scene
+{
+    constructor ()
+    {
+        super();
+
+        this.create();
+    }
+
+    async create ()
+    {
+        await ImageFile('logo', 'assets/logo.png');
+
+        const world = new StaticWorld(this);
+
+        const dd = new DirectDraw();
+
+        dd.render = () =>
+        {
+            dd.fillRect(100, 100, 340, 240, 0xff0000);
+            dd.fillRect(200, 200, 512, 64, 0xffff00, 0.6);
+            dd.fillRect(300, 300, 48, 210, 0xff00ff);
+        };
+
+        AddChild(world, dd);
+    }
+}
+
+new Game(
+    WebGL(),
+    Parent('gameParent'),
+    GlobalVar('Phaser4'),
+    BackgroundColor(0x2d2d2d),
+    Scenes(Demo)
+);
---
+++ examples/src/gameobjects/directdraw/layered drawing.ts
@@ -0,0 +1,79 @@
+import { BackgroundColor, GlobalVar, Parent, Scenes, WebGL } from '../../../../../phaser-genesis/src/config';
+import { DirectDraw, Sprite } from '../../../../../phaser-genesis/src/gameobjects';
+
+import { AddChildren } from '../../../../../phaser-genesis/src/display';
+import { Game } from '../../../../../phaser-genesis/src/Game';
+import { ImageFile } from '../../../../../phaser-genesis/src/loader/files';
+import { Scene } from '../../../../../phaser-genesis/src/scenes/Scene';
+import { StaticWorld } from '../../../../../phaser-genesis/src/world/StaticWorld';
+
+class Demo extends Scene
+{
+    constructor ()
+    {
+        super();
+
+        this.create();
+    }
+
+    async create ()
+    {
+        await ImageFile('logo', 'assets/logo.png').load();
+        await ImageFile('brain', 'assets/brain.png').load();
+
+        const world = new StaticWorld(this);
+
+        const logo = new Sprite(400, 90, 'logo');
+        const brain = new Sprite(400, 300, 'brain');
+
+        const dd = new DirectDraw();
+
+        const distance = 300;
+        const speed = 6;
+        
+        const max = 400;
+        const xx = [];
+        const yy = [];
+        const zz = [];
+        
+        for (var i = 0; i < max; i++)
+        {
+            xx[i] = Math.floor(Math.random() * 800) - 400;
+            yy[i] = Math.floor(Math.random() * 600) - 300;
+            zz[i] = Math.floor(Math.random() * 1700) - 100;
+        }
+    
+        dd.render = () =>
+        {
+            dd.rect(100, 100, 600, 400, 0x000000, 0.5);
+            dd.box(100, 100, 600, 400, 0xffffff);
+
+            for (let i = 0; i < max; i++)
+            {
+                const perspective = distance / (distance - zz[i]);
+
+                const x = 400 + xx[i] * perspective;
+                const y = 300 + yy[i] * perspective;
+        
+                zz[i] += speed;
+        
+                if (zz[i] > 300)
+                {
+                    zz[i] -= 600;
+                }
+
+                dd.plot(x, y, 0xffffff);
+            }
+        };
+
+        AddChildren(world, logo, dd, brain);
+    }
+}
+
+new Game(
+    WebGL(),
+    Parent('gameParent'),
+    GlobalVar('Phaser4'),
+    BackgroundColor(0x2d2d2d),
+    Scenes(Demo)
+);
