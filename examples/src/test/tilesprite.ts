52a176fdc9b8ad6627f0b1059720d292664ec3f6
Richard Davey <rich@photonstorm.com>
2021-09-30 18:47:03 +0100

    Tilesprites working

--- examples/src/test/tilesprite.ts
+++ examples/src/test/tilesprite.ts
@@ -4,9 +4,10 @@ import { BackgroundColor, GlobalVar, Parent, Scenes, WebGL } from '../../../../p
 
 import { AddChild } from '../../../../phaser-genesis/src/display';
 import { Game } from '../../../../phaser-genesis/src/Game';
+import { LoadAtlasFile } from '../../../../phaser-genesis/src/loader/files/LoadAtlasFile';
 import { LoadImageFile } from '../../../../phaser-genesis/src/loader/files/LoadImageFile';
+import { LoadSpriteSheetFile } from '../../../../phaser-genesis/src/loader/files/LoadSpriteSheetFile';
 import { Scene } from '../../../../phaser-genesis/src/scenes/Scene';
-import { SetFrameUVs } from '../../../../phaser-genesis/src/textures/SetFrameUVs';
 import { StaticWorld } from '../../../../phaser-genesis/src/world/StaticWorld';
 import { TileSprite } from '../../../../phaser-genesis/src/gameobjects/tilesprite/TileSprite';
 
@@ -21,17 +22,23 @@ class Demo extends Scene
 
     async create ()
     {
-        await LoadImageFile('512', 'assets/fantasy-tiles.png', { glConfig: { wrapS: GL_CONST.REPEAT, wrapT: GL_CONST.REPEAT, flipY: true }});
+        const pixelArt = { glConfig: { minFilter: GL_CONST.NEAREST, magFilter: GL_CONST.NEAREST }};
+
+        await LoadImageFile('cat', 'assets/orange-cat1.png', pixelArt);
+        await LoadAtlasFile('atlas', 'assets/atlas-trimmed.png', 'assets/atlas-trimmed.json', pixelArt);
+        await LoadAtlasFile('atlasnotrim', 'assets/atlas-notrim.png', 'assets/atlas-notrim.json', pixelArt);
+        await LoadSpriteSheetFile('grid', 'assets/gridtiles.png', { frameWidth: 32, frameHeight: 32 }, pixelArt);
 
         const world = new StaticWorld(this);
 
-        const bob = new TileSprite(400, 300, '512');
+        const bob = new TileSprite(20, 20, 'cat');
 
-        bob.rotation = 0.2;
+        // const bob = new TileSprite(20, 20, 'atlas', 'brain');
+        // const bob = new TileSprite(20, 20, 'atlasnotrim', 'brain');
+        // const bob = new TileSprite(20, 20, 'grid', 30);
 
-        setInterval(() => {
-
-        }, 100);
+        bob.tileScale.set(4, 2);
+        // bob.rotation = 0.2;
 
         AddChild(world, bob);
     }
